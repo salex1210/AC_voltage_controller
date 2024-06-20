a6a0950f7df87c46be871ae7123e59517b764d27
Gerson Alexander Sanchez Brito <54285794+salex1210@users.noreply.github.com>
2024-06-19 20:37:57 -0500

    codigo original

--- sketch_may9a/sketch_may9a.ino
+++ sketch_may9a/sketch_may9a.ino
@@ -1,29 +1,19 @@
-const int crucePorCeroPin = 2;  // Pin conectado al detector de cruce por cero
-const int salidaPin = 3;         // Pin donde se enviará el pulso después del cruce por cero
-volatile boolean cruce_cero = 0;
-void setup() {
-  pinMode(crucePorCeroPin, INPUT);  // Establece el pin de cruce por cero como entrada
-  pinMode(salidaPin, OUTPUT);        // Establece el pin de salida como salida
-  attachInterrupt(0,deteccion_cruce_cero,RISING);
-}
+const int crucePorCeroPin = 2;  
+const int salidaPin = 3;         
 
-void deteccion_cruce_cero(){
-  cruce_cero = true;
-  digitalWrite(salidaPin, LOW);
+void setup() {
+  pinMode(crucePorCeroPin, INPUT);  
+  pinMode(salidaPin, OUTPUT);        
 }
 
 void loop() {
-  // Espera hasta que se detecte un cruce por cero
   while(digitalRead(crucePorCeroPin) == LOW) {
-    delayMicroseconds(10); // Pequeña pausa para evitar el uso excesivo de recursos
+    delayMicroseconds(10); 
   }
   
-  // Espera 2.33ms (2330 microsegundos) después del cruce por cero
-  delayMicroseconds(2566);
+  delayMicroseconds(2576);
   
-  // Envía un pulso en el pin de salida
   digitalWrite(salidaPin, HIGH);
-  delayMicroseconds(10);  // Duración del pulso (opcional, ajusta según sea necesario)
-  
+  delayMicroseconds(10);  
+  digitalWrite(salidaPin, LOW);
 }
-
